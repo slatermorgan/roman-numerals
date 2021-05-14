ea6c5fc5746d24b1a909d8747111c2aa4e260b6d
Morgan Slater <45826559+slatermorgan@users.noreply.github.com>
2021-05-14 18:57:11 +0100

    Style Pages

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <!DOCTYPE HTML>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+</head>
 <html>
     <body>
-        <div>
-            <h1>
-                Convert an integer
-            </h1>
-            <h2>
-                between 1 - 100000
-            </h2>
-            <h2>
-                to a roman numeral
-            </h2>
-            <form action="result.php" method="post">
-                Integer: <input type="text" name="int">
-                <br>
-                <input type="submit">
-            </form>
-        </div>
-
+    <div class="jumbotron">
+        <h1 class="display-4">Roman Numeral Converter</h1>
+        <p class="lead">Convert an integer between 1 - 100000 to a roman numeral</p>
+        <form action="result.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="int">
+                <small id="emailHelp" class="d-none">We'll never share your email with anyone else.</small>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     </body>
 </html>
--- result.php
+++ result.php
@@ -5,8 +5,25 @@ use RomanNumeral\Helper\RomanNumeralConverter;
 
 ?>
 
+<!DOCTYPE HTML>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+</head>
 <html>
     <body>
-        Welcome <?php echo RomanNumeralConverter::getRomanNumeral($_POST["int"]); ?><br>
+        <div class="jumbotron">
+            <h1 class="display-4">Roman Numeral Converter</h1>
+            <p class="lead">Convert an integer between 1 - 100000 to a roman numeral</p>
+            <div class="form-group">
+                <span>
+                    Result:
+                    <strong>
+                        <?php echo RomanNumeralConverter::getRomanNumeral($_POST["int"]); ?>
+                    </strong>
+                </span>
+                <small id="emailHelp" class="d-none">We'll never share your email with anyone else.</small>
+            </div>
+            <a href="/" class="btn btn-primary">Try Another</a>
+        </div>
     </body>
 </html>
